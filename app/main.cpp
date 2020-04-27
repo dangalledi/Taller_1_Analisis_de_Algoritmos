6061f89baf8a9c2995c0c190b06b25c19b7569cd
Daniela Galleguillos <dangalledi@gmail.com>
2020-04-26 20:42:06 -0400

    ADD: se agregan cositas y se ordena :)

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <string.h>
 
 
 using namespace std;
@@ -21,28 +22,46 @@ int main( int argc, char* argv[] ) {
      * Incluir acá la lógica del programa
      * 
      */
+
+    string argumento = argv[1];
+
     if (argc > 1) {
         // @TODO programar proyecto
-        if (argv[ 1 ] == a){
+        if (argv[ 1 ] == 'a'){
             cout << "The name used to start the program: " << argv[ 0 ]
                 << "\nArguments are:\n";
-            for (int n = 1; n < argc; n++)
-                cout << n << ": " << argv[ n ] << '\n';
-                if (argv[ n ] == 'a' || argv[ n ] == 'b' || argv[ n ] == 'o' || argv[ n ] == '∗'  ) {
-
-                }
+            
         }if( argc[1]! == 'a'){
-            cout << "No cumple la condición"
+            cout << "No cumple la condición";
     }else {
         // Mostrar los integrantes
         participantes();
     }
     return EXIT_SUCCESS;
 }
+};
 
 void participantes() {
     std::cout << std::endl << "=== Taller 01 ===" << std::endl;
     std::cout << std::endl << "Edgar Matus"; 
     std::cout << std::endl << "Oscar Peñaloza"; 
     std::cout << std::endl << "Daniela Galleguillos" << std::endl; 
+};
+
+bool ValidacionDelLenguaje(string argumento[] ){
+
+int validacionLargoArgumento = 0;
+
+    for (int n = 1; n < argumento.length(); n++)
+        cout << n << ": " << argumento[ n ] << '\n';
+        if (argumento[ n ] == 'a' || argumento[ n ] == 'b' || argumento[ n ] == 'o' || argumento[ n ] == '∗'  ) {
+            cout<< "Holiiii" << endl;
+            validacionLargoArgumento++;        
+            }
+    }
+    if (validacionLargoArgumento == argumento.length() ){
+        return true;
+    }else{
+        return false;
+    }
 }
