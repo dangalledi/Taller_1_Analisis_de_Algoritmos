6f8d546dc4b17fccb9559265f94efa9745d457d9
Daniela Galleguillos <dangalledi@gmail.com>
2020-04-27 22:35:49 -0400

    CHG: Se ordena el codigo en funciones

--- app/main.cpp
+++ app/main.cpp
@@ -2,8 +2,17 @@
 #include <cstdlib>
 #include <string.h>
 
-
 using namespace std;
+
+/**
+ * Función donde valida los caracteres del lenguaje
+ */
+bool ValidacionDelLenguaje( string argumento );
+
+/**
+ * Función donde validan las condiciones 
+ */
+bool CondicionDelLenguaje( string argumento );
 
 /**
  * Función que muestra los participantes del grupo
@@ -11,34 +20,25 @@ using namespace std;
 void participantes();
 
 /**
- * Función funcion donde valida los caracteres del lenguaje
- */
-bool ValidacionDelLenguaje( string argumento );
-
-/**
  * Taller computacional
  * @param argc cantidad de argumentos
  * @param argv argumentos
  * @return El código de salida del programa
  */
+
 int main( int argc, char* argv[] ) {
 
     string argumento = argv[1];  // char -> string
 
-    if (argc > 1) {
+    if (argc == 2) {
         // @TODO programar proyecto
-        if (argumento[ 0 ] != 'a'){   // validacion de entrada x empiezacon"a" 
+        if ( ValidacionDelLenguaje( argumento ) == false ){   // validacion del lenguaje a , b , o , *
             cout << "No cumple la condición" << endl ;
             return 0;
         }else {
-            for (size_t n = 1; n < argumento.length(); n++){
-                if ( ValidacionDelLenguaje( argumento ) == false ){  // a , b , o , *
-                    cout << "No cumple la condición" << endl ;
-                    return 0;
-                }if (argumento[n]=='*'){  //Validacion de contenido no contiene a "*"
-                    cout << "No cumple la condición" << endl ;
-                    return 0;
-                }
+            if ( CondicionDelLenguaje( argumento ) == false ){  // Validacion de las condiciones 
+                cout << "No cumple la condición" << endl ;
+                return 0;
             }
             cout << "Palabra valida: "<< argv[1] << endl;
             return EXIT_SUCCESS;
@@ -51,16 +51,32 @@ int main( int argc, char* argv[] ) {
     return EXIT_SUCCESS;
 };
 
-
 bool ValidacionDelLenguaje( string argumento ){
-    for (size_t n = 1; n < argumento.length(); n++){
-        if (argumento[ n ] == 'a' || argumento[ n ] == 'b' || argumento[ n ] == 'o' || argumento[ n ] == '∗'  ) 
-            return true;
-        else 
-            return false;
 
+    int contador =0;
+
+    for (size_t n = 0; n < argumento.length(); n++){
+        if (argumento[ n ] == 'a' || argumento[ n ] == 'b' || argumento[ n ] == 'o' || argumento[ n ] == '∗'  ) 
+            contador++;
     }
+    if (contador == argumento.length())
+        return true;
+    else 
+        return false;
 };
+
+bool CondicionDelLenguaje(string argumento){
+  if (argumento[0]=='a')
+    {
+    for(size_t n =1; n < argumento.length()-1; n++){
+      if(argumento[n] == '*')
+        return false;
+      }
+    }
+  else 
+    return false;
+  return true;
+}
 
 void participantes() {
     std::cout << std::endl << "=== Taller 01 ===" << std::endl;
